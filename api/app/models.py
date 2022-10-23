6e958ccfeafb3450f4bf6ef596558962c0b5afec
lc-code20 <62159627+lc-code20@users.noreply.github.com>
2022-10-23 16:43:23 +0100

    add QuestionSet and UserSet to models

--- api/app/models.py
+++ api/app/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
@@ -11,5 +12,21 @@ class User(AbstractUser):
 class Question(models.Model):
     question = models.TextField()
     answer = models.CharField(max_length=10)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+
+    #user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     
+class QuestionSet(models.Model): 
+    accessCode = models.CharField(max_length=255) 
+    questions = models.ManyToManyField(Question)
+
+class ResponseOption(models.Model): 
+    text = models.CharField(max_length=255) 
+
+class UserQuestionSet(models.Model): 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    questionSet = models.ForeignKey(QuestionSet, on_delete=models.CASCADE)
+ 
+class UserResponse(models.Model): 
+    user_set = models.ForeignKey(UserQuestionSet, on_delete=models.CASCADE) 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    response_option = models.ForeignKey(ResponseOption, on_delete=models.CASCADE) 
